70152b726b81e457c59c58e0ad7456b82a5cccb4
Yefferson326 <yefferson.nunez@uptc.edu.co>
2023-10-20 11:37:02 -0500

    Agregando funcion de recepcion

--- main.py
+++ main.py
@@ -47,10 +47,43 @@ def division_xor(divisor, dividendo):
     #print("Mensaje Enviado",msn_alter)
     return msn
 
+def recepcion_mensaje(divisor,mensaje):
+    
 
-def pedir_datos():
+    divisor = np.array(list(divisor[2:]), dtype=int)
+    dividendo = mensaje
+
+    print("Mensaje recibido:", dividendo)
+    
+    # #! Se hace la division XOR
+    
+    for i in range(len(dividendo) - len(divisor) + 1):
+        if dividendo[i] == 1:
+            dividendo[i:i + len(divisor)] = dividendo[i:i + len(divisor)] ^ divisor
+        else:
+            dividendo[i:i + len(divisor)] = dividendo[i:i + len(divisor)] ^ np.zeros(len(divisor), dtype=int)
+        print(dividendo)
+    adding = dividendo[-len(divisor):]
+    
+
+    # msn = np.concatenate((dividendo_ref, adding))  # * Se concatena el mensaje con el residuo
+    # # msn_alter=alterar_mensaje(msn)
+
+    # # !Imprimimos Pruebas de valores
+    # # print("Residuo: ",add)
+    # # print("Divisor: ",divisor)
+    # # print("Divisor: ",dividendo)
+    # # print("Residuo: ",residuo)
+    # # print("Addicionar",adding)
+    # # print("Mensaje Original",msn)
+    # # print("Mensaje Enviado",msn_alter)
+    # return msn
+
+
+
+def pedir_datos(generador):
     trama = bin(int(input("Ingrese el mensaje que desee enviar "), 2))
-    generator = "0b101"
+    generator = generador
     mensaje=division_xor(generator, trama)
     return mensaje
 
@@ -60,10 +93,12 @@ def pedir_datos():
         # print("El mensaje enviado es: ", sent)
         #print("Mensaje Enviado",mensaje)
 def main():
-    mensaje=pedir_datos()
+    generator="0b101"
+    mensaje=pedir_datos(generator)
     mensaje_alter=alterar_mensaje(mensaje)
     print("Mensaje Enviado",mensaje)
     print("Mensaje Alterado",mensaje_alter)
+    recepcion_mensaje(generator,mensaje_alter )
 
 
 if __name__ == "__main__":
